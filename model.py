aecc776306ccf4034c51f522c89fcb64183667f0
Robert Yaw Agyekum Addo <300621923815@bluecrest.edu.gh>
2023-12-19 10:19:47 -0800

    Update model.py
    Layer normalization included in the transformer model

--- model.py
+++ model.py
@@ -36,3 +36,15 @@ class PositionalEncoding(nn.Model):
         x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
         #x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
+    
+class LayerNormalization(nn.Module):
+    def __init__(self, eps: float = 10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) # Multiplied
+        self.bias = nn.Parameter(torch.zeros(1)) # Added
+        
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim = True)
+        std = x.std(dim = -1, keepdim = True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.alpha
