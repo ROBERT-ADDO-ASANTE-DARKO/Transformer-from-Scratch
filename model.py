98e83ead5241c4731dbc4721fe550c7b272dd851
Robert Yaw Agyekum Addo <300621923815@bluecrest.edu.gh>
2023-12-19 12:29:04 -0800

    Included the feed forward layer in the transformer model 	modified:   model.py

--- model.py
+++ model.py
@@ -48,3 +48,14 @@ class LayerNormalization(nn.Module):
         mean = x.mean(dim = -1, keepdim = True)
         std = x.std(dim = -1, keepdim = True)
         return self.alpha * (x - mean) / (std + self.eps) + self.alpha
+    
+class FeedForwardBlock(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model) # W1 and B1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model) # W2 and B2
+        
+    def forward(self, x):
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, d_ff) --> (Batch, seq_len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
